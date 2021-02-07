00533b28a83ffca70b4d72a1466499d66407d78d
dern123 <58784052+dern123@users.noreply.github.com>
2021-02-08 01:56:05 +0200

    Update Main.java

--- services/api/src/main/java/io/swec/services/api/Main.java
+++ services/api/src/main/java/io/swec/services/api/Main.java
@@ -19,8 +19,7 @@ public class Main {
 
     // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://0.0.0.0:9082/api";
-//
-    //
+
     // Sonar issue
     // String uname = "steve";
     // String password = "blue";
